5506b3a2d46a3a8f596c558bb33f7502e39ba1d9
vm-packages <vm-packages@google.com>
2025-02-03 11:50:22 +0000

    Add js-deobfuscator.vm

--- packages/js-deobfuscator.vm/tools/chocolateyinstall.ps1
+++ packages/js-deobfuscator.vm/tools/chocolateyinstall.ps1
@@ -3,5 +3,6 @@ Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'js-deobfuscator'
 $category = 'Javascript'
+$arguments = '-h'
 
-VM-Install-Node-Tool -toolName $toolName -category $category -arguments "--help"
+VM-Install-Node-Tool -toolName $toolName -category $category -arguments $arguments
