8fcc461f909dc7e8b83870ff6ce53d0b69417983
Sara Rincon <sararn@google.com>
2025-02-13 13:36:44 +0100

    :qModify packages from f to j
    Add the category to the nuspec file

--- packages/hasher.vm/tools/chocolateyinstall.ps1
+++ packages/hasher.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'Hasher'
-$category = 'File Information'
+$category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 $zipUrl = 'https://download.mikestammer.com/hasher.zip'
 $zipSha256 = '14ee103793fae4f165adc5e0a9424ca75ea0a4dc2e823dcc2b7cdeb2ae94483c'
--- packages/jlecmd.vm/tools/chocolateyinstall.ps1
+++ packages/jlecmd.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'JLECmd'
-$category = 'Forensic'
+$category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 $zipUrl = 'https://download.mikestammer.com/net6/JLECmd.zip'
 $zipSha256 = 'b0635517a72d2a7cdfdc92d5161f38e968380ae2ec33673571108bacf31b4480'
